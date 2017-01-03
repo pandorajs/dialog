aeff4be567f114f06721010597af9a82db080ad0
lynzz <lynzz168@gmail.com>
2017-01-03 11:12:29 +0800

    删除无用的

--- _site/dialog-cov.js
+++
@@ -1,9 +0,0 @@
-
-var __cov_g2KzdQxS$xDYHQwz$SHObA = (Function('return this'))();
-if (!__cov_g2KzdQxS$xDYHQwz$SHObA.__coverage__) { __cov_g2KzdQxS$xDYHQwz$SHObA.__coverage__ = {}; }
-__cov_g2KzdQxS$xDYHQwz$SHObA = __cov_g2KzdQxS$xDYHQwz$SHObA.__coverage__;
-if (!(__cov_g2KzdQxS$xDYHQwz$SHObA['E:\\cms3\\v3\\spm3\\dialog\\dialog.js'])) {
-   __cov_g2KzdQxS$xDYHQwz$SHObA['E:\\cms3\\v3\\spm3\\dialog\\dialog.js'] = {"path":"E:\\cms3\\v3\\spm3\\dialog\\dialog.js","s":{"1":0,"2":0,"3":0,"4":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":41}},"2":{"start":{"line":2,"column":0},"end":{"line":2,"column":44}},"3":{"start":{"line":3,"column":0},"end":{"line":3,"column":50}},"4":{"start":{"line":4,"column":0},"end":{"line":4,"column":46}}},"branchMap":{}};
-}
-__cov_g2KzdQxS$xDYHQwz$SHObA = __cov_g2KzdQxS$xDYHQwz$SHObA['E:\\cms3\\v3\\spm3\\dialog\\dialog.js'];
-__cov_g2KzdQxS$xDYHQwz$SHObA.s['1']++;module.exports=require('./lib/dialog');__cov_g2KzdQxS$xDYHQwz$SHObA.s['2']++;module.exports.Tips=require('./lib/tips');__cov_g2KzdQxS$xDYHQwz$SHObA.s['3']++;module.exports.Confirm=require('./lib/confirm');__cov_g2KzdQxS$xDYHQwz$SHObA.s['4']++;module.exports.Alert=require('./lib/alert');
--- _site/dialog.js
+++
@@ -1,4 +0,0 @@
-module.exports = require('./lib/dialog');
-module.exports.Tips = require('./lib/tips');
-module.exports.Confirm = require('./lib/confirm');
-module.exports.Alert = require('./lib/alert');
